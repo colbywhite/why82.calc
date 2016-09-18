43156d648713e327d5c8288004cd17490a7e73e8
Colby M. White <colbywhite@users.noreply.github.com>
2016-09-17 21:23:52 -0500

    combine multiple days into one dict

--- calc_schedule.py
+++ calc_schedule.py
@@ -7,10 +7,14 @@ import why82.schedule as sked
 
 # noinspection PyUnusedLocal
 def lambda_handler(event, context):
-    calc_schedule(_date.today(), 0)
+    calc_schedule(_date.today())
 
 
-def calc_schedule(start_date, offset):
+def calc_schedule(start_date):
+    return sked.get_seven_day_schedule(start_date)
+
+
+def calc_schedule_offset(start_date, offset):
     return sked.get_schedule(start_date, offset)
 
 
@@ -24,4 +28,5 @@ if __name__ == '__main__':
     parser.add_argument('--date', '-d', default=_date.today(), type=date, help='The start date to grab a schedule for. '
                                                                                'DEFAULT=date.today()')
     args = parser.parse_args()
-    print(calc_schedule(args.date, args.offset))
+    # print(calc_schedule_offset(args.date, args.offset))
+    print(calc_schedule(args.date))
--- why82/schedule/__init__.py
+++ why82/schedule/__init__.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import requests
 
 NBA_SCOREBOARD_URL = 'http://stats.nba.com/stats/scoreboardV2'
@@ -6,12 +8,29 @@ NBA_API_HEADERS = {'Referer': 'http://stats.nba.com/scores/', 'User-Agent': 'cur
 
 
 def get_seven_day_schedule(start_date):
-    return {get_schedule(start_date, n) for n in range(0, 6)}
+    return reduce(lambda result, offset:
+                  dict(result.items() + get_schedule(start_date, offset).items()),
+                  range(0, 6),
+                  {})
 
 
 def get_schedule(start_date, offset):
     params = {'GameDate': start_date.strftime('%Y-%m-%d'), 'LeagueID': NBA_LEAGUE_ID, 'DayOffset': offset}
     response = requests.get(NBA_SCOREBOARD_URL, params=params, headers=NBA_API_HEADERS)
     response.raise_for_status()
-    games = response.json()['resultSets'][0]['rowSet']
-    return {game[5] for game in games}
+    game_strings = response.json()['resultSets'][0]['rowSet']
+    date = parse_game_date(game_strings[0][5])
+    games = map(lambda x: parse_game_teams(x[5]), game_strings)
+    return {date: games}
+
+
+def parse_game_teams(game_info):
+    teams = game_info.split('/')[1]
+    away_abbr = teams[:3]
+    home_abbr = teams[3:]
+    return {'home': home_abbr, 'away': away_abbr}
+
+
+def parse_game_date(game_info):
+    date_string = game_info.split('/')[0]
+    return datetime.strptime(date_string, '%Y%m%d').date()
