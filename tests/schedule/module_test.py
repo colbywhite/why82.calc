6f44b0804987f908ca5a4686a9a7761cf5e7331a
Colby M. White <colbywhite@users.noreply.github.com>
2016-09-18 21:01:42 -0500

    add tests for graded sked

--- tests/schedule/module_test.py
+++ tests/schedule/module_test.py
@@ -1,4 +1,4 @@
-from datetime import date
+from datetime import date, timedelta
 from unittest import TestCase
 
 from nose.tools import *
@@ -39,3 +39,31 @@ class GetScheduleTest(TestCase):
             games = schedule[day]
             expected_games = utils.get_json_resource(name)
             eq_(games, expected_games)
+
+
+class GradeScheduleTest(TestCase):
+    def setUp(self):
+        # build three days of schedule
+        start_date = date(2016, 2, 3)
+        self.date_strings = []
+        self.schedule = {}
+        for offset in range(0, 3):
+            cur_date = start_date + timedelta(days=offset)
+            self.date_strings.append(cur_date.strftime('%Y-%m-%d'))
+            name = 'NBA_schedule_%s_%d' % (start_date.strftime('%Y-%m-%d'), offset)
+            self.schedule[cur_date] = utils.get_json_resource(name)
+        self.tiers = utils.get_json_resource('NBA_2016-tiers')
+        self.graded_schedule = sked.grade_schedule(self.schedule, self.tiers)
+
+    def test_game_counts(self):
+        eq_(11, len(self.graded_schedule['2016-02-03']))
+        eq_(4, len(self.graded_schedule['2016-02-04']))
+        eq_(10, len(self.graded_schedule['2016-02-05']))
+
+    def test_game_contents(self):
+        games = self.graded_schedule['2016-02-04']
+        game = [g for g in games if g['home']['abbreviated_name'] == 'DET']
+        eq_(1, len(game))
+        game = game[0]
+        eq_('DET', game['home']['abbreviated_name'])
+        eq_('NYK', game['away']['abbreviated_name'])
