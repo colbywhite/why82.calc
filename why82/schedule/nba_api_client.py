d87cbbe0da8fc4bbac7dff39ef540470c62edf03
Colby M. White <colbywhite@users.noreply.github.com>
2017-01-21 13:36:29 -0800

    correcting print statement

--- why82/schedule/nba_api_client.py
+++ why82/schedule/nba_api_client.py
@@ -7,7 +7,7 @@ NBA_API_HEADERS = {'Referer': 'http://stats.nba.com/scores/', 'User-Agent': 'cur
 
 def v2scoreboard(start_date, offset):
     params = {'GameDate': start_date.strftime('%Y-%m-%d'), 'LeagueID': NBA_LEAGUE_ID, 'DayOffset': offset}
-    print("hitting %s with params" % (NBA_SCOREBOARD_URL, params))
+    print("hitting %s with params %s" % (NBA_SCOREBOARD_URL, params))
     response = requests.get(NBA_SCOREBOARD_URL, params=params, headers=NBA_API_HEADERS)
     print("received response")
     response.raise_for_status()
