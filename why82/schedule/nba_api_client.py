1b033cc7497d93c83b5619d8c4abea132b5dd9f5
Colby M. White <colbywhite@users.noreply.github.com>
2017-03-18 14:21:15 -0700

    correcting user agent
    once again, the nba api is acting picky. it seems to hang on the
    previous user agent. this user agent was poached from the `nba.json`
    from nickb1080/nba-client-template@aa89032

--- why82/schedule/nba_api_client.py
+++ why82/schedule/nba_api_client.py
@@ -2,8 +2,8 @@ import requests
 
 NBA_SCOREBOARD_URL = 'http://stats.nba.com/stats/scoreboardV2'
 NBA_LEAGUE_ID = '00'
-NBA_API_HEADERS = {'Referer': 'http://stats.nba.com/scores/', 'User-Agent': 'curl/7.43.0'}
-
+NBA_USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/45.0.2454.101 Safari/537.36'
+NBA_API_HEADERS = {'Referer': 'http://stats.nba.com/scores/', 'User-Agent': NBA_USER_AGENT}
 
 def v2scoreboard(start_date, offset):
     params = {'GameDate': start_date.strftime('%Y-%m-%d'), 'LeagueID': NBA_LEAGUE_ID, 'DayOffset': offset}
